21259808812d95999ece5c732c15c7a9bdfcd8d9
Fabio Prestes <fpcrezende@gmail.com>
2023-12-13 13:32:04 -0400

    feature: photo_couple_list.dart and respective function

---
+++ lib/widgets/photo_couple_list.dart
@@ -0,0 +1,37 @@
+import 'package:app_skeleton/models/image.dart';
+import 'package:flutter/material.dart';
+
+class PhotoCoupleList extends StatelessWidget{
+  const PhotoCoupleList({super.key, required this.photoCouples});
+
+  final List<PhotoCouple> photoCouples;
+
+  @override
+  Widget build(BuildContext context) {
+    if (photoCouples.isEmpty) {
+      return Center(
+        child: Text(
+          'No photos added yet',
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      );
+    }
+
+    return  ListView.builder(
+          itemCount: photoCouples.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: CircleAvatar(
+                radius: 26,
+                backgroundImage: FileImage(photoCouples[index].imagePano),
+              ),
+              title: Text(photoCouples[index].id),
+              subtitle: Text('Sent at ${DateTime.now()}'),
+              trailing: const Icon(Icons.file_upload_off),
+            );
+          },
+        );
+  }
+}

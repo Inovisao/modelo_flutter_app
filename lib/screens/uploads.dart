443d6d775ab21db8617bc60d5a0e49a584d9b22c
Fabio Prestes <fpcrezende@gmail.com>
2023-12-13 16:21:25 +0000

    Revert "soon-to-be-reverted"
    This reverts commit 5f4d45707a0e84ace2af8408a94df2b6720d09d4

--- lib/screens/uploads.dart
+++ lib/screens/uploads.dart
@@ -1,50 +1,41 @@
 import 'package:app_skeleton/models/image.dart';
-import 'package:app_skeleton/providers/user_images.dart';
 import 'package:app_skeleton/screens/choose_image.dart';
-import 'package:app_skeleton/widgets/photo_couple_list.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class UploadsScreen extends ConsumerStatefulWidget {
+class UploadsScreen extends StatefulWidget {
   const UploadsScreen({super.key});
 
   @override
-  ConsumerState<UploadsScreen> createState() {
+  State<UploadsScreen> createState() {
     return _UploadsScreenState();
   }
 }
 
-class _UploadsScreenState extends ConsumerState<UploadsScreen> {
-  late Future<void> _photosFuture;
-  @override
-  void initState() {
-    super.initState();
-    _photosFuture = ref.read(userImagesProvider.notifier).loadPhotos();
-  }
+class _UploadsScreenState extends State<UploadsScreen> {
+  // TODO: initState to read notifier and loadPhotos() that have not been uploaded yet
 
   //final _items = List<String>.generate(10, (index) => 'Item ${index + 1}');
   final _allCouples = [];
 
   void _addPhotos() async {
-    final newPhotos = await Navigator.of(context).push<PhotoCouple>(
+    final newPhotoCouple = await Navigator.of(context).push<PhotoCouple>(
       MaterialPageRoute(
         builder: (ctx) => const ImagePickerScreen(),
       ),
     );
 
-    if (newPhotos == null) {
+    if (newPhotoCouple == null) {
       return;
     }
 
     setState(() {
-      _allCouples.add(newPhotos);
+      _allCouples.add(newPhotoCouple);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    final userPhotos = ref.watch(userImagesProvider);
-
+    //TODO: create variable to watch notifier,use it for FutureBuilder
     void _removePhotos(String item) {
       final index = _allCouples.indexOf(item);
 
@@ -67,19 +58,28 @@ class _UploadsScreenState extends ConsumerState<UploadsScreen> {
           ),
         ], //TODO: force upload
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: FutureBuilder(
-          future: _photosFuture,
-          builder: (context, snapshot) =>
-              snapshot.connectionState == ConnectionState.waiting
-                  ? const Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : PhotoCoupleList(
-                      photoCouples: userPhotos,
-                    ),
-        ),
+      body: ListView.builder(
+        itemCount: _allCouples.length,
+        itemBuilder: (context, index) {
+          return Dismissible(
+            onDismissed: (direction) {
+              _removePhotos(_allCouples[index]);
+              print(_allCouples);
+            },
+            key: Key(
+              _allCouples[index],
+            ),
+            child: ListTile(
+              leading: const CircleAvatar(
+                radius: 26,
+                child: Icon(Icons.photo),
+              ),
+              title: Text(_allCouples[index]),
+              subtitle: const Text('Photo creation date'),
+              trailing: const Icon(Icons.file_upload_off),
+            ),
+          );
+        },
       ),
     );
   }

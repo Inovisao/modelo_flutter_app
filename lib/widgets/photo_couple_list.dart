a58e40e00ea13c885660bfcef568d9e3895bf59d
Fabio Prestes <fpcrezende@gmail.com>
2024-01-30 09:21:10 -0400

    Converted PhotoCoupleList to Stateful

--- lib/widgets/photo_couple_list.dart
+++ lib/widgets/photo_couple_list.dart
@@ -1,14 +1,19 @@
 import 'package:app_skeleton/models/image.dart';
 import 'package:flutter/material.dart';
 
-class PhotoCoupleList extends StatelessWidget{
+class PhotoCoupleList extends StatefulWidget {
   const PhotoCoupleList({super.key, required this.photoCouples});
 
   final List<PhotoCouple> photoCouples;
 
   @override
+  State<PhotoCoupleList> createState() => _PhotoCoupleListState();
+}
+
+class _PhotoCoupleListState extends State<PhotoCoupleList> {
+  @override
   Widget build(BuildContext context) {
-    if (photoCouples.isEmpty) {
+    if (widget.photoCouples.isEmpty) {
       return Center(
         child: Text(
           'No photos added yet',
@@ -19,19 +24,41 @@ class PhotoCoupleList extends StatelessWidget{
       );
     }
 
-    return  ListView.builder(
-          itemCount: photoCouples.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: CircleAvatar(
-                radius: 26,
-                backgroundImage: FileImage(photoCouples[index].imagePano),
-              ),
-              title: Text(photoCouples[index].id),
-              subtitle: Text('Sent at ${photoCouples[index].creationDate}'),
-              trailing: photoCouples[index].isUploaded==0 ? const Icon(Icons.file_upload_off) : const Icon(Icons.file_upload),
-            );
-          },
+    return ListView.builder(
+      itemCount: widget.photoCouples.length,
+      itemBuilder: (context, index) {
+        return Dismissible(
+          key: Key(widget.photoCouples[index].id),
+          background: Container(color: Colors.red),
+          onDismissed: (direction) {},
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 26,
+              backgroundImage: FileImage(widget.photoCouples[index].imagePano),
+            ),
+            title: Text(widget.photoCouples[index].id),
+            subtitle:
+                Text('Sent at ${widget.photoCouples[index].creationDate}'),
+            trailing: widget.photoCouples[index].isUploaded == 0
+                ? const Icon(Icons.file_upload_off)
+                : const Icon(Icons.file_upload),
+          ),
         );
+      },
+    );
+    // return ListView.builder(
+    //       itemCount: photoCouples.length,
+    //       itemBuilder: (context, index) {
+    //         return ListTile(
+    //           leading: CircleAvatar(
+    //             radius: 26,
+    //             backgroundImage: FileImage(photoCouples[index].imagePano),
+    //           ),
+    //           title: Text(photoCouples[index].id),
+    //           subtitle: Text('Sent at ${photoCouples[index].creationDate}'),
+    //           trailing: photoCouples[index].isUploaded==0 ? const Icon(Icons.file_upload_off) : const Icon(Icons.file_upload),
+    //         );
+    //       },
+    //     );
   }
 }
